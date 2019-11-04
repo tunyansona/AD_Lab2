393b46072b95cfdc6d88fe874ce4dae75c9a7cb3
CatherineMathieu <CMathieu6@users.noreply.github.com>
2019-11-05 00:47:09 +0100

    Add abstract class AbstractST

---
+++ lab2_prep/src/AbstractST.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public abstract class AbstractST<Key, Val> implements Iterable<Key> {
+    // abstract methods
+    abstract public boolean isEmpty();
+    abstract public void put(Key key, Val val);
+    abstract public Val get(Key key);
+    abstract public void delete(Key key);
+    abstract public Iterator<Key> iterator();
+
+    public Boolean contains(Key key) {
+        return (get(key) != null);
+    }
+
+    public void initWith(Key[] karr, Val[] varr) { };
+}
