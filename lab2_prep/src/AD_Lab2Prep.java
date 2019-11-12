7a8790c27f8a06c4ebae213ce4caf7237367659e
CatherineMathieu <CMathieu6@users.noreply.github.com>
2019-11-12 17:47:13 +0100

    Improve main code

--- lab2_prep/src/AD_Lab2Prep.java
+++ lab2_prep/src/AD_Lab2Prep.java
@@ -1,12 +1,20 @@
 import treeviewer.BinaryTreeViewer;
+
 import java.util.Random;
 
 public class AD_Lab2Prep {
-    public static void main(String args[]) {
-        Random rnd = new Random(42);
-        BinarySearchTree<Integer, Integer> bst = new BinarySearchTree<Integer, Integer>();
+    public static void main(String[] args) {
 
-        System.out.println("Binary Search Tree");
+        BinarySearchTree<Integer, Integer> bst = new BinarySearchTree<Integer, Integer>();
+        displayDataBinaryTree(bst, "Binary Search Tree");
+
+        RandomizedBinarySearchTree<Integer, Integer> rbst = new RandomizedBinarySearchTree<Integer, Integer>();
+        displayDataBinaryTree(rbst, "Randomized Binary Search Tree");
+    }
+
+    public static void displayDataBinaryTree(BinarySearchTree<Integer, Integer> bst, String name) {
+        Random rnd = new Random(42);
+        System.out.println("\n" + name);
         for (int i = 0; i < 10; i++) {
             int k = rnd.nextInt(100);
             bst.put(k, i);
@@ -15,37 +23,14 @@ public class AD_Lab2Prep {
         for (int i = 0; i < 100; i++) {
             Integer v = bst.get(i);
             if (v != null) {
-                System.out.println("got k = " + i + " and value " + v);
+                System.out.println("Node with key " + i + " has value " + v);
             }
         }
 
-        System.out.println(bst.getHeight());
-        System.out.println(bst.getAverageDistance());
+        System.out.println("Tree height: " + bst.getHeight());
+        System.out.println("Average Distance: " + bst.getAverageDistance());
 
-        BinaryTreeViewer btv1 = new BinaryTreeViewer();
-        btv1.setTree(bst);
-
-
-        // Test Randomized Binary Search Tree
-        RandomizedBinarySearchTree<Integer, Integer> rbst = new RandomizedBinarySearchTree<Integer, Integer>();
-
-        System.out.println("Randomized Binary Search Tree");
-        for (int i = 0; i < 10; i++) {
-            int k = rnd.nextInt(100);
-            rbst.put(k, i);
-            System.out.println("Put (" + k + ", " + i + ")");
-        }
-        for (int i = 0; i < 100; i++) {
-            Integer v = rbst.get(i);
-            if (v != null) {
-                System.out.println("got k = " + i + " and value " + v);
-            }
-        }
-
-        System.out.println(rbst.getHeight());
-        System.out.println(rbst.getAverageDistance());
-
-        BinaryTreeViewer btv2 = new BinaryTreeViewer();
-        btv2.setTree(rbst);
+        BinaryTreeViewer btv = new BinaryTreeViewer();
+        btv.setTree(bst);
     }
 }
